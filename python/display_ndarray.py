c4ae616f4393bd6811673fa4da42b4af528ee8be
codrelphi <chancerel.codjovi@gmail.com>
2019-10-08 22:28:43 +0100

    create the file display_ndarray.py and add my personal header

---
+++ python/display_ndarray.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-08
+# source: https://stackoverflow.com/questions/58289625/how-to-get-the-same-output-using-multidimensional-arrays/
+#=================================================================================
