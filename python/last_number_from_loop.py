d1d0cde634814378dd5cbef7a4be486d273fc358
codrelphi <chancerel.codjovi@gmail.com>
2019-10-11 14:42:48 +0100

    write my code

--- python/last_number_from_loop.py
+++ python/last_number_from_loop.py
@@ -5,3 +5,15 @@
 # date: 2019-10-11
 # source: https://stackoverflow.com/questions/58342215/getting-the-last-number-from-a-for-loop/58342301
 #=================================================================================
+
+# use a list
+def LCM(minN,maxN):
+    count = 1
+    results = []
+    for i in range(count,(maxN*count)+1):
+        results.append(minN*count)
+        count = count + 1
+    print(results[-1]) # print the last elements of the list.
+
+
+LCM(5, 7) # print 35.
