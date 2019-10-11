51cf4645a5d1b8b56d50429255e20ab1831adb3f
codrelphi <chancerel.codjovi@gmail.com>
2019-10-11 14:42:31 +0100

    create the file and add my personal header

---
+++ python/last_number_from_loop.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-11
+# source: https://stackoverflow.com/questions/58342215/getting-the-last-number-from-a-for-loop/58342301
+#=================================================================================
