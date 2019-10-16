de0443496cfe64a9d35b7078ddbef19f82bcf615
codrelphi <chancerel.codjovi@gmail.com>
2019-10-16 13:51:12 +0100

    create the file split_by_keyword.py and add my personal header

---
+++ python/split_by_keyword.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-16
+# source: https://stackoverflow.com/questions/58412309/how-can-i-split-the-second-line-when-the-keyword-match-two-lines-by-python/
+#=================================================================================
