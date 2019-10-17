ac99feb47f02aa5d017c280827b2559a4f903cac
codrelphi <chancerel.codjovi@gmail.com>
2019-10-17 16:03:05 +0100

    create the file and add my personal header

---
+++ python/columns_filtering_printing.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-17
+# source: https://stackoverflow.com/questions/58415096/python-if-else-condition-on-printing-lines/
+#=================================================================================
