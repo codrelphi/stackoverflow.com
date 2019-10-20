93983738febb50ad22e906c6df917665ad71cf98
codrelphi <chancerel.codjovi@gmail.com>
2019-10-20 20:41:44 +0100

    add my personal header

---
+++ python/assert_dict_key.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-20
+# source: https://stackoverflow.com/questions/58475838/check-a-dict-key-using-assert-from-unit-test
+#=================================================================================
