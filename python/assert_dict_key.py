ee4e4812f41528befff4bee736133a039550844d
codrelphi <chancerel.codjovi@gmail.com>
2019-10-20 20:42:45 +0100

    write the code to achive the goal

--- python/assert_dict_key.py
+++ python/assert_dict_key.py
@@ -5,3 +5,9 @@
 # date: 2019-10-20
 # source: https://stackoverflow.com/questions/58475838/check-a-dict-key-using-assert-from-unit-test
 #=================================================================================
+
+d1 = {'success': 'success', 'return message': 'return_msg', 'debug_data': 'debug_data'}
+d2 =  {'success': 'validation', 'return message': 'return_msg', 'debug_data': 'debug_data'}
+
+# If the values are not the same, you will get the exception AssertionError.
+assert d1['success'] == d2['success']
