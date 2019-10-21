6a77fba6f6ad0580b66678433ac4783f53fb18ec
codrelphi <chancerel.codjovi@gmail.com>
2019-10-21 22:58:38 +0100

    add a comment about the task

--- python/swap_first_last_digit.py
+++ python/swap_first_last_digit.py
@@ -5,3 +5,4 @@
 # date: 2019-10-21
 # source: https://stackoverflow.com/questions/58494338/swap-first-and-last-digits-of-a-number-using-loops
 #=================================================================================
+""" Task: Swap the first and the last digit of a number using loop """
