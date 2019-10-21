233fed8df6e837798d9af3b4a889f6a352720d93
codrelphi <chancerel.codjovi@gmail.com>
2019-10-21 22:57:48 +0100

    add my personal header

---
+++ python/swap_first_last_digit.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-21
+# source: https://stackoverflow.com/questions/58494338/swap-first-and-last-digits-of-a-number-using-loops
+#=================================================================================
