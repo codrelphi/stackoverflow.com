966994f4c6e27070549960b394ce1bc32c56aaa2
codrelphi <chancerel.codjovi@gmail.com>
2019-10-23 23:11:53 +0100

    add my personal header

---
+++ python/compute_type_user_input.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-23
+# source: https://stackoverflow.com/questions/58530668/how-to-find-type-of-user-input-and-print-different-values-depending-on-the-type/
+#=================================================================================
