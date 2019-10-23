dfd334b1d4b83f03927db8b60c12fd3659b9b765
codrelphi <chancerel.codjovi@gmail.com>
2019-10-23 23:13:21 +0100

    write the code

--- python/compute_type_user_input.py
+++ python/compute_type_user_input.py
@@ -5,3 +5,24 @@
 # date: 2019-10-23
 # source: https://stackoverflow.com/questions/58530668/how-to-find-type-of-user-input-and-print-different-values-depending-on-the-type/
 #=================================================================================
+
+def findt():
+    userin = input("Input: ")
+
+    if userin.isnumeric():
+        # userin is numeric
+        result = float(userin) ** 2
+        print(result)
+
+    else:
+        try:
+            # userin is a float
+            result = float(userin) ** 2
+            print(result)
+        except ValueError:
+            # userin is a string
+            print("Sorry Dave, I'm afraid I can't do that")
+            return 0.0
+
+
+findt()
