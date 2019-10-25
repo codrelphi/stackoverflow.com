4370a9ae36030e9f8933b110353538dd55bda492
codrelphi <chancerel.codjovi@gmail.com>
2019-10-25 02:32:44 +0100

    write the code using a list

--- python/counting_letters.py
+++ python/counting_letters.py
@@ -6,8 +6,21 @@
 # source: https://stackoverflow.com/questions/58550800/adding-the-amount-of-letters-there-are-in-a-string-in-python
 #=================================================================================
 
-# Method 1: using a list
+# Method 2: using a counter
+def countingletters(st):
+    cpt = 0 # the counter
+    for i in st:
+        if i.isalpha():
+            cpt += 1
+    return cpt
 
+
+# test
+print(countingletters("a12a")) # display 2
+
+
+"""
+# Method 1: using a list
 def countingletters(st):
     empty = []
     for i in st:
@@ -18,3 +31,4 @@ def countingletters(st):
 
 # test
 print(countingletters("a12a")) # display 2
+"""
