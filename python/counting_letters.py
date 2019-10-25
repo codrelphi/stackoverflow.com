d26227aa44875178ec0673d306b3d90381bd401a
codrelphi <chancerel.codjovi@gmail.com>
2019-10-25 02:29:52 +0100

    add my personal header

---
+++ python/counting_letters.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-25
+# source: https://stackoverflow.com/questions/58550800/adding-the-amount-of-letters-there-are-in-a-string-in-python
+#=================================================================================
