f0e21ad080445213fb888eee5ad893f28399e318
codrelphi <chancerel.codjovi@gmail.com>
2019-10-26 03:46:17 +0100

    add my personal header

---
+++ python/remove_triplicate_letters.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-26
+# source: https://stackoverflow.com/questions/58565315/how-to-remove-triplicate-letters-in-python/
+#=================================================================================
