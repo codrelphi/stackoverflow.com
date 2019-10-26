ab204d6f41b436c277fe22067404d227ea0c464d
codrelphi <chancerel.codjovi@gmail.com>
2019-10-26 03:47:10 +0100

    write the code using list

--- python/remove_triplicate_letters.py
+++ python/remove_triplicate_letters.py
@@ -5,3 +5,31 @@
 # date: 2019-10-26
 # source: https://stackoverflow.com/questions/58565315/how-to-remove-triplicate-letters-in-python/
 #=================================================================================
+def removeTriplicateLetters(sentence):
+    """
+        @param: sentence -> the sentence to transform
+        @param: words -> the words in the sentence
+        @param: new_words -> list of the final words of the new sentence
+
+    """
+    words = sentence.split(" ") # split the sentence into words
+    new_words = []
+    for word in words:  # loop through words of the sentence
+        new_word = []
+        for char in word:   # loop through characters in a word
+            position = word.index(char)
+            if word.count(char) >= 3:
+                new_word = [i for i in word if i != char]
+                new_word.insert(position, char)
+        new_words.append(''.join(new_word))
+
+    return ' '.join(new_words)
+
+
+def main():
+    print(removeTriplicateLetters('byeee mmmy friiiennd'))
+
+
+main()
+
+# output: 'bye my friennd'
