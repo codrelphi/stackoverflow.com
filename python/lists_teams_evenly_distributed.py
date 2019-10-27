618af70927bf4f35df547a9d88dfcc7d3db6727d
codrelphi <chancerel.codjovi@gmail.com>
2019-10-27 21:12:02 +0100

    add my personal header

---
+++ python/lists_teams_evenly_distributed.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-27
+# source: https://stackoverflow.com/questions/58576153/assigning-values-of-one-list-to-another-list-with-even-distribution
+#=================================================================================
