be10b3cc59650395ec35a102e0dd43ef9657873b
codrelphi <chancerel.codjovi@gmail.com>
2019-10-28 21:11:15 +0100

    add my personal header

---
+++ python/bytes_decode_assignment.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-20
+# source: https://stackoverflow.com/questions/58477936/importing-arduino-byte-data-into-python3-using-readline-want-to-assign-data-t
+#=================================================================================
