db64c1849e140e8d30e946a59d77b26b154b4e37
codrelphi <chancerel.codjovi@gmail.com>
2019-10-28 21:11:55 +0100

    write the working code

--- python/bytes_decode_assignment.py
+++ python/bytes_decode_assignment.py
@@ -5,3 +5,14 @@
 # date: 2019-10-20
 # source: https://stackoverflow.com/questions/58477936/importing-arduino-byte-data-into-python3-using-readline-want-to-assign-data-t
 #=================================================================================
+
+while True:
+    data = ser.readline()
+    data = data.strip().decode('utf-8') # to decode byte to unicode
+    data = data.split(',') # you get something like ['10.1', '20.2', '30.3']
+    a = float(data[0]) # transform '10.1' to 10.1
+    b = float(data[1]) # transform '20.2' to 20.2
+    c = float(data[2]) # transform '30.3' to 30.3
+    print(a) # display 10.1
+    print(b) # display 20.2
+    print(c) # display 30.2
