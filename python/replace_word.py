9aa705bad11cdc6497425ae1364352c4dff790f8
codrelphi <chancerel.codjovi@gmail.com>
2019-10-29 22:40:40 +0100

    add the working code

--- python/replace_word.py
+++ python/replace_word.py
@@ -5,3 +5,19 @@
 # date: 2019-10-29
 # source:
 #=================================================================================
+
+def replaceWord(sep, word, sentence):
+    """
+        @param: sep is the separator
+        @param: word is the work to replace
+        @param: sentence is the sentence
+    """
+    len_word = len(word)
+    replacer = sep * len_word
+    print(sentence.replace(word, replacer))
+
+sentence = "what is tomorrow's date?"
+word = "what"
+sep = "-"
+replaceWord(sep, word, sentence)
+#print(help(replaceWord))
