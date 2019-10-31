2899f019030d71436c2174ab5f08a686be676197
codrelphi <chancerel.codjovi@gmail.com>
2019-10-31 23:57:19 +0100

    add my personal header

---
+++ python/populate_tuple_with_strings.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-31
+# source: https://stackoverflow.com/questions/58643815/populating-a-python-tuple-with-strings-stored-in-different-keys-across-multiple
+#=================================================================================
