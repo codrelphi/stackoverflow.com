1025c7008dbd85c0696494fef76087c9a25b5f1a
codrelphi <chancerel.codjovi@gmail.com>
2019-10-31 23:58:17 +0100

    describe the problem and the desired output

--- python/populate_tuple_with_strings.py
+++ python/populate_tuple_with_strings.py
@@ -5,3 +5,16 @@
 # date: 2019-10-31
 # source: https://stackoverflow.com/questions/58643815/populating-a-python-tuple-with-strings-stored-in-different-keys-across-multiple
 #=================================================================================
+
+""" The problem:
+
+Suppose I have the following data structures:
+
+x = {'name': 'x', 'alias': ['test']}
+y = {'name': 'y', 'alias': ['run', 'log']}
+z = (x, y)
+And now I would like to populate a tuple with all of the name and alias strings, like so:
+
+result = ('x', 'y', 'test', 'run', 'log')
+
+"""
