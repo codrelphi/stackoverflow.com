8071f4d9d84818aa01d681bb4fba6f208ba96e1c
codrelphi <chancerel.codjovi@gmail.com>
2019-11-02 22:33:24 +0100

    add my personal header

---
+++ python/buy_products.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-02
+# source: https://stackoverflow.com/questions/58675306/increasing-a-key-by-knowing-the-value-python-dictionary/
+#=================================================================================
