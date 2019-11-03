7b832fb87982b3e095b0af9200f17537b1fdbe13
codrelphi <chancerel.codjovi@gmail.com>
2019-11-03 22:00:53 +0100

    write the working code

--- python/find_lists_without_elements.py
+++ python/find_lists_without_elements.py
@@ -8,3 +8,9 @@
 
 # Problem
 # Find list elements that do not contain any of the characters ( . - \ / )
+
+book_list = ["book1", "book.2", "book/3", "book/\4", "book-5", "book(6", "book)7", "book(8)", "book10"]
+
+for wo2 in book_list:
+    if "/" not in wo2 and "\"" not in wo2 and "-" not in wo2 and "." not in wo2 and "(" not in wo2 and ")" not in wo2:
+        print (wo2)
