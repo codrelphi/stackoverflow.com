10d282981a015f3fdb3f8c148b1a4176f8e6c6a0
codrelphi <chancerel.codjovi@gmail.com>
2019-11-03 21:58:32 +0100

    add my personal header

---
+++ python/find_lists_without_elements.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-03
+# source: https://stackoverflow.com/questions/58683846/find-list-elements-that-do-not-contain-any-of-the-characters
+#=================================================================================
