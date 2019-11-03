a93299c20e83339c84c6b759dab8632ad2b71002
codrelphi <chancerel.codjovi@gmail.com>
2019-11-03 21:59:46 +0100

    States the problem

--- python/find_lists_without_elements.py
+++ python/find_lists_without_elements.py
@@ -5,3 +5,6 @@
 # date: 2019-11-03
 # source: https://stackoverflow.com/questions/58683846/find-list-elements-that-do-not-contain-any-of-the-characters
 #=================================================================================
+
+# Problem
+# Find list elements that do not contain any of the characters ( . - \ / )
