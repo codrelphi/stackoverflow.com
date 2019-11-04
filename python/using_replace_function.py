cb9b6b4d603c59e4e69953e20e91a938630ffa38
codrelphi <chancerel.codjovi@gmail.com>
2019-11-04 20:34:08 +0100

    write the code to achieve the goal

--- python/using_replace_function.py
+++ python/using_replace_function.py
@@ -5,3 +5,16 @@
 # date: 2019-11-04
 # source: https://stackoverflow.com/questions/58653327/using-replace-function/
 #=================================================================================
+curseWords = ["crap", "butt", "fork"]
+niceWords = ["poo", "buttox", "spoon"]
+dirtySentence = "You crap, butt in fork"
+
+def Censor(curseWords, niceWords, dirtySentence):
+    for i in range(len(niceWords)):
+        dirtySentence = dirtySentence.replace(curseWords[i], niceWords[i])
+
+    return dirtySentence
+
+
+dirtySentence = Censor(curseWords, niceWords, dirtySentence) # call the defined function
+print(dirtySentence)
