32d804e44ae825e8b1ff48b956a254e1e0cfb99b
codrelphi <chancerel.codjovi@gmail.com>
2019-11-06 23:55:54 +0100

    reorganize the code

--- python/buy_products.py
+++ python/buy_products.py
@@ -6,17 +6,18 @@
 # source: https://stackoverflow.com/questions/58675306/increasing-a-key-by-knowing-the-value-python-dictionary/
 #=================================================================================
 
-demand = {"bread":0,"butter":0,"cheese":0, "water":0,"ice cream":0}
-# bread is the value and 0 is the key, which I want to increase every time
-
 def bill(*buy_lst):  # see this line
     for item in buy_lst:
         demand[item] += 1  # see this line
 
-bill("bread", "cheese") # The client buys the products `bread` and `cheese`
-print(demand)
-bill("bread", "cheese", "water", "butter")
-print(demand)
+if __name__ == "__main__":
+    demand = {"bread":0,"butter":0,"cheese":0, "water":0,"ice cream":0}
+    # bread is the value and 0 is the key, which I want to increase every time
+
+    bill("bread", "cheese") # The client buys the products `bread` and `cheese`
+    print(demand)
+    bill("bread", "cheese", "water", "butter")
+    print(demand)
 
 # Outputs
 # {'bread': 1, 'butter': 0, 'cheese': 1, 'water': 0, 'ice cream': 0}
