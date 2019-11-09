fab7804b8bcaee05f2701a229c79d182df5a9a34
codrelphi <chancerel.codjovi@gmail.com>
2019-11-09 16:53:52 +0100

    describe the problem

--- python/remove_elements_from_list.py
+++ python/remove_elements_from_list.py
@@ -5,3 +5,11 @@
 # date: 2019-11-09
 # source: https://stackoverflow.com/questions/58780726/remove-elements-from-list-in-python-if-it-contains-in-other-lists
 #=================================================================================
+
+"""The elements in the main list should be removed if they are present in either of the other two lists.
+Example:
+s1 = [1,2,3,4,7]
+s2 = [3,4,5,6,20]
+mainlist = [6,7,8,9,10,11,12,13,14,15]
+resultList = [8,9,10,11,12,13,14,15]
+"""
