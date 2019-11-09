f0ce33855d6c9fbd445516bcb88f0b82d832c530
codrelphi <chancerel.codjovi@gmail.com>
2019-11-09 16:51:43 +0100

    add my personal header

---
+++ python/remove_elements_from_list.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-09
+# source: https://stackoverflow.com/questions/58780726/remove-elements-from-list-in-python-if-it-contains-in-other-lists
+#=================================================================================
