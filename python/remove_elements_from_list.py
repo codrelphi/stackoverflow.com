38ebd56e68a4767b76c7937c14b88262c2e43226
codrelphi <chancerel.codjovi@gmail.com>
2019-11-09 16:55:29 +0100

    solve the problem using for-loop

--- python/remove_elements_from_list.py
+++ python/remove_elements_from_list.py
@@ -13,3 +13,13 @@ s2 = [3,4,5,6,20]
 mainlist = [6,7,8,9,10,11,12,13,14,15]
 resultList = [8,9,10,11,12,13,14,15]
 """
+
+s1 = [1,2,3,4,7]
+s2 = [3,4,5,6,20]
+mainlist = [6,7,8,9,10,11,12,13,14,15]
+
+for i in mainlist:
+    if i in s1 or i in s2:
+        mainlist.remove(i)
+
+print(mainlist)
