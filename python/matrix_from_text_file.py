238330bfdfd42158c8791f2e0acbb6bfbd0ea0a9
codrelphi <chancerel.codjovi@gmail.com>
2019-11-10 20:36:08 +0100

    add my personal header

---
+++ python/matrix_from_text_file.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-10
+# source: https://stackoverflow.com/questions/58789668/how-to-create-a-matrix-from-a-given-text-file/
+#=================================================================================
