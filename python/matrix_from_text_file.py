c558558630615307cdc3c221f697a2dfbaa65d2e
codrelphi <chancerel.codjovi@gmail.com>
2019-11-10 20:38:05 +0100

    describe the problem

--- python/matrix_from_text_file.py
+++ python/matrix_from_text_file.py
@@ -5,3 +5,13 @@
 # date: 2019-11-10
 # source: https://stackoverflow.com/questions/58789668/how-to-create-a-matrix-from-a-given-text-file/
 #=================================================================================
+
+""" matrix.txt
+xxxxx
+xS--x
+xx-Fx
+xxxxx
+"""
+# desired output
+# print(matrix[1][1])
+# S
