fe887ce5656d59b6c566ac78bedd8c17ddf09c26
codrelphi <chancerel.codjovi@gmail.com>
2019-11-11 20:45:11 +0100

    add my personal header

---
+++ python/sort_list_data_by_time.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-11
+# source: https://stackoverflow.com/questions/58804838/is-there-an-obvious-way-to-sort-this-list-of-data-by-time-in-python
+#=================================================================================
