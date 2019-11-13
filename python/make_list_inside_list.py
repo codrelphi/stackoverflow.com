8d391e6a76e8b1cf5ddbb9fc98bc301e4479fd0a
codrelphi <chancerel.codjovi@gmail.com>
2019-11-13 21:08:36 +0100

    describe the problem

--- python/make_list_inside_list.py
+++ python/make_list_inside_list.py
@@ -5,3 +5,6 @@
 # date: 2019-11-13
 # source: https://stackoverflow.com/questions/58844128/make-a-list-inside-of-a-list-using-loops
 #=================================================================================
+
+# Problem
+# factorial_list(4) yields [[1], [1, 2], [1, 2, 3], [1, 2, 3, 4]]
