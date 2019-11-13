756ef207e255717e102e86414fe0e4269261cd7b
codrelphi <chancerel.codjovi@gmail.com>
2019-11-13 21:09:50 +0100

    solution based on a mix of for loop and list-comprehensions

--- python/make_list_inside_list.py
+++ python/make_list_inside_list.py
@@ -12,10 +12,8 @@
 def factorial_list(n):
     final_list = []
     for i in range(1, n+1):
-        temp = []
-        for j in range(1, i+1):
-            temp.append(j)
+        temp = [j for j in range(1, i+1)]
         final_list.append(temp)
     print(final_list)
 
-factorial_list(4)   # [[1], [1, 2], [1, 2, 3], [1, 2, 3, 4]]
+factorial_list(4)  # [[1], [1, 2], [1, 2, 3], [1, 2, 3, 4]]
