bcf94d8ee1d4d3e800f17f618faae3c4c01b441e
codrelphi <chancerel.codjovi@gmail.com>
2019-11-13 21:07:57 +0100

    add my personal header

---
+++ python/make_list_inside_list.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-13
+# source: https://stackoverflow.com/questions/58844128/make-a-list-inside-of-a-list-using-loops
+#=================================================================================
