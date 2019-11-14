22198b4aec3f285b9aca64269e8e575172013195
codrelphi <chancerel.codjovi@gmail.com>
2019-11-14 16:39:56 +0100

    add my personal header

---
+++ python/count_char_in_text.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-14
+# source: https://stackoverflow.com/questions/58860125/is-there-a-way-to-count-the-occurrence-of-a-character-in-each-word-of-a-string-i/
+#=================================================================================
