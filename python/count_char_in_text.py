9954235a0b9782997e00b1f993c94814ef724194
codrelphi <chancerel.codjovi@gmail.com>
2019-11-14 16:40:52 +0100

    write the code using a list-comprehension

--- python/count_char_in_text.py
+++ python/count_char_in_text.py
@@ -5,3 +5,13 @@
 # date: 2019-11-14
 # source: https://stackoverflow.com/questions/58860125/is-there-a-way-to-count-the-occurrence-of-a-character-in-each-word-of-a-string-i/
 #=================================================================================
+
+def count_char(text, char):
+    text = text.lower()
+    char = char.lower()
+    words = text.split()
+    return [word.count(char) for word in words]
+
+
+if __name__ == '__main__':
+    print(count_char("Welcome to this world", "w")) # [1, 0, 0, 1]
