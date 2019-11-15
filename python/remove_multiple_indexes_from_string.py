1b79650d7fa1a96647de0d156c1fcb3d5af64fce
codrelphi <chancerel.codjovi@gmail.com>
2019-11-15 23:48:39 +0100

    add the working code

--- python/remove_multiple_indexes_from_string.py
+++ python/remove_multiple_indexes_from_string.py
@@ -5,3 +5,11 @@
 # date: 2019-11-15
 # source: https://stackoverflow.com/questions/58858763/can-i-remove-multiple-indexes-from-a-string-with-a-for-loop
 #=================================================================================
+
+text = "Gvusaibdsz8audvbsauzdgsavuczisagbcsuzaicbhas"
+text_list = list(text)
+for index in range(0, 7):
+    text_list.remove(text[index])
+
+text = ''.join(text_list)
+print(text)   # dsz8audvbsauzdgsavuczisagbcsuzaicbhas
