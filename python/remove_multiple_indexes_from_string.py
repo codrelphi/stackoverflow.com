943f0085f999863928d9dd5b685686f46a96dc85
codrelphi <chancerel.codjovi@gmail.com>
2019-11-15 23:47:01 +0100

    add my personal header

---
+++ python/remove_multiple_indexes_from_string.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-15
+# source: https://stackoverflow.com/questions/58858763/can-i-remove-multiple-indexes-from-a-string-with-a-for-loop
+#=================================================================================
