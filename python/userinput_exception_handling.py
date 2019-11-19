7a0d2f1aab2d56f45e197589663fe4b84a89fe1c
codrelphi <chancerel.codjovi@gmail.com>
2019-11-20 00:33:21 +0100

    add my personal header

---
+++ python/userinput_exception_handling.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-20
+# source: https://stackoverflow.com/questions/58924586/exception-handling-easy-python
+#=================================================================================
