403b9191e588ffd7c2f1fa6962b9a06494d76c5d
codrelphi <chancerel.codjovi@gmail.com>
2019-11-20 00:33:48 +0100

    write the working code

--- python/userinput_exception_handling.py
+++ python/userinput_exception_handling.py
@@ -5,3 +5,18 @@
 # date: 2019-11-20
 # source: https://stackoverflow.com/questions/58924586/exception-handling-easy-python
 #=================================================================================
+
+def is_perfect(n):
+    total = 0
+    for x in range(1, n):
+        if n % x == 0:
+            total = total + x
+    return total == n
+
+
+if __name__ == '__main__':
+    try:
+        user_input = int(input("Enter your number: "))
+        print(is_perfect(user_input))
+    except ValueError:
+        print("Error: enter a number")
