0aec2974e6188e1b10bb5b691f75835d35268216
codrelphi <chancerel.codjovi@gmail.com>
2019-11-21 00:36:44 +0100

    add the problem

--- python/extract_chars_from_string_using_split.py
+++ python/extract_chars_from_string_using_split.py
@@ -5,3 +5,8 @@
 # date: 2019-11-21
 # source: https://stackoverflow.com/questions/58964513/splitting-and-appending-a-string-in-python
 #=================================================================================
+
+"""
+I want to take the state number 01 the county number 001 and the tract 020100
+and make a new string 01001020100. How do I achieve this in Python?
+"""
