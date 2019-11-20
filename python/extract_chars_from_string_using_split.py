f960eb79728bd864c6c2be3612cc2b6e87091325
codrelphi <chancerel.codjovi@gmail.com>
2019-11-21 00:36:14 +0100

    add my personal header

---
+++ python/extract_chars_from_string_using_split.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-21
+# source: https://stackoverflow.com/questions/58964513/splitting-and-appending-a-string-in-python
+#=================================================================================
