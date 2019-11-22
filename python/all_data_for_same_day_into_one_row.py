02c028cf04b5fb2406ca4a5a7c0b8ee81ca6f7c8
codrelphi <chancerel.codjovi@gmail.com>
2019-11-22 23:58:11 +0100

    add my personal header

---
+++ python/all_data_for_same_day_into_one_row.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-22
+# source: https://stackoverflow.com/questions/58902138/gather-all-data-for-same-day-into-one-row/
+#=================================================================================
