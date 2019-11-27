3816f4fa3a5c8337a4daf3393c0b336154fff820
codrelphi <chancerel.codjovi@gmail.com>
2019-11-27 23:19:30 +0100

    write the working code

--- python/update_an_initialized_variable_in_python_class.py
+++ python/update_an_initialized_variable_in_python_class.py
@@ -5,3 +5,23 @@
 # date: 2019-11-27
 # source: https://stackoverflow.com/questions/59040973/how-do-i-update-an-initialized-variable-used-in-python-class/
 #=================================================================================
+
+class Whatever:
+    def __init__(self, thing1):
+        self.list=[]
+        self.thing=thing1
+
+    def list_append(self):
+        df1=pd.read_csv('job.py')
+        df2=pd.read_csv('home.py')
+
+        self.list.append([df1,df2])
+        return self.list
+
+    def new_function(self):
+        self.list_append() # to call the above method
+        return self.list
+
+function=Whatever(thing1)
+function_output=function.new_function()
+print(function_output)
