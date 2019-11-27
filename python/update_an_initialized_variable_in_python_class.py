33ce7cd66522c9320e8e3bef766ddd6e4969e813
codrelphi <chancerel.codjovi@gmail.com>
2019-11-27 23:18:48 +0100

    add my personal header

---
+++ python/update_an_initialized_variable_in_python_class.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-11-27
+# source: https://stackoverflow.com/questions/59040973/how-do-i-update-an-initialized-variable-used-in-python-class/
+#=================================================================================
