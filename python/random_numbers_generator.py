29bda5ddba3c87d7958b7c947c36ebd3e2ca3b8e
codrelphi <chancerel.codjovi@gmail.com>
2019-12-01 19:34:48 +0100

    write the working code using "for loop"

--- python/random_numbers_generator.py
+++ python/random_numbers_generator.py
@@ -5,3 +5,15 @@
 # date: 2019-12-01
 # source: https://stackoverflow.com/questions/59128392/random-numbers-exercise
 #=================================================================================
+
+import random
+
+def rolldies(n):
+    result = []
+    for i in range(n):
+        number = random.randrange(1, 6)
+        result.append(number)
+    return sorted(result)
+
+
+print(rolldies(3)) # display [3, 4, 5] for example
