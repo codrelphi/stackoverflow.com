e033e11b671018bc88cf23855b301ec98fdd6f3b
codrelphi <chancerel.codjovi@gmail.com>
2019-12-01 19:35:26 +0100

    write the working code using a list-comprehension

--- python/random_numbers_generator.py
+++ python/random_numbers_generator.py
@@ -5,15 +5,10 @@
 # date: 2019-12-01
 # source: https://stackoverflow.com/questions/59128392/random-numbers-exercise
 #=================================================================================
-
 import random
 
 def rolldies(n):
-    result = []
-    for i in range(n):
-        number = random.randrange(1, 6)
-        result.append(number)
-    return sorted(result)
+    return sorted([random.randrange(1, 6) for i in range(n)])
 
 
-print(rolldies(3)) # display [3, 4, 5] for example
+print(rolldies(3)) # display [2, 4, 5] for example
