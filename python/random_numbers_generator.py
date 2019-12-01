1d6032c83b9e40ba241d0cb84c3780870f76acd4
codrelphi <chancerel.codjovi@gmail.com>
2019-12-01 19:31:59 +0100

    add my personal header

---
+++ python/random_numbers_generator.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-12-01
+# source: https://stackoverflow.com/questions/59128392/random-numbers-exercise
+#=================================================================================
