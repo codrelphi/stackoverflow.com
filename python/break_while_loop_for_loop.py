7309a9b004780af504ae02fa15a96d160409cc7c
codrelphi <chancerel.codjovi@gmail.com>
2019-12-03 02:31:08 +0100

    write the working code

--- python/break_while_loop_for_loop.py
+++ python/break_while_loop_for_loop.py
@@ -5,3 +5,21 @@
 # date: 2019-12-03
 # source: https://stackoverflow.com/questions/59148762/how-can-i-properly-use-while-loop-and-not-have-it-repeat-on-break
 #=================================================================================
+
+# break will exit from the for loop
+# you need a flag to exit from the while loop
+
+looper = True # the flag
+while looper:
+profile_name = input("\nEnter Profile Name (Case Sensitive): ")
+    if len(profile_name) == 0:
+        print("Profile cannot be blank!\n")
+        pass
+    for profile in prof_collection.find({"name": profile_name}):
+        if profile_name in profile["name"]:
+            print("%s has been found." % profile_name)
+            looper = False # flag changes
+            break
+        else:
+            print("%s was not found. Please enter a valid Profile." % profile_name)
+            pass
