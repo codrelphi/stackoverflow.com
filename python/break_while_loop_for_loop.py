4c192763a4992abc572f5d212689ad76a7a4d853
codrelphi <chancerel.codjovi@gmail.com>
2019-12-03 02:29:50 +0100

    add my personal header

---
+++ python/break_while_loop_for_loop.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-12-03
+# source: https://stackoverflow.com/questions/59148762/how-can-i-properly-use-while-loop-and-not-have-it-repeat-on-break
+#=================================================================================
