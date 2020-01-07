c32d473dc1fdbdd5e55e62918145c2841983e062
codrelphi <chancerel.codjovi@gmail.com>
2020-01-07 17:23:15 +0100

    add my personal header

---
+++ python/year_printing_errors.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2020-01-07
+# source: https://stackoverflow.com/questions/59624974/if-function-doesnt-return-anything-in-python
+#=================================================================================
