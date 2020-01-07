41d737391c6d6c5d29415bfdbe38af3b69d193e9
codrelphi <chancerel.codjovi@gmail.com>
2020-01-07 17:24:14 +0100

    add a comment to explain the problem and the solution

--- python/year_printing_errors.py
+++ python/year_printing_errors.py
@@ -5,3 +5,17 @@
 # date: 2020-01-07
 # source: https://stackoverflow.com/questions/59624974/if-function-doesnt-return-anything-in-python
 #=================================================================================
+"""
+Your function returns the year. So you have to print the result of the function calls.
+To do that, just replace the following lines:
+
+century(1601)
+century(58)
+century(356)
+
+by these lines:
+
+print(century(1601))   # 2
+print(century(58))     # 1
+print(century(356))    # 57
+"""
