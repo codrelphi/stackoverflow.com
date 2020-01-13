d5d084d2f49c550968edf78de739599bd5a0b1d5
codrelphi <chancerel.codjovi@gmail.com>
2020-01-13 08:13:32 +0100

    add my personal header

---
+++ python/average_dictionary_members.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2020-01-13
+# source: https://stackoverflow.com/questions/59705517/attempting-to-get-the-average-from-numbers-in-a-dictionary
+#=================================================================================
