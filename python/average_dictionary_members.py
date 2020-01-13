652e9cf7ff31531555e009f81e5c1aaa94c10ca1
codrelphi <chancerel.codjovi@gmail.com>
2020-01-13 08:15:14 +0100

    write the working code

--- python/average_dictionary_members.py
+++ python/average_dictionary_members.py
@@ -5,3 +5,19 @@
 # date: 2020-01-13
 # source: https://stackoverflow.com/questions/59705517/attempting-to-get-the-average-from-numbers-in-a-dictionary
 #=================================================================================
+
+def get_average(numbers):
+    """compute the average of the dictionary members."""
+    val = 0
+    for v in numbers.values():
+        val += v
+    avg = val / len(numbers)
+    return avg
+
+if __name__ == '__main__':
+    numbers = {
+        'one': 1,
+        'two': 2
+    }
+
+    print(get_average(numbers)) # 1.5
