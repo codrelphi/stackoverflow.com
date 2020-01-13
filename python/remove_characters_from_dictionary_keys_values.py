e529af6709f4e0304710670c68894f888b467d46
codrelphi <chancerel.codjovi@gmail.com>
2020-01-13 22:21:38 +0100

    add my personal header

---
+++ python/remove_characters_from_dictionary_keys_values.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2020-01-13
+# source: https://stackoverflow.com/questions/59723285/removing-characters-from-both-a-dictionarys-keys-and-values
+#=================================================================================
