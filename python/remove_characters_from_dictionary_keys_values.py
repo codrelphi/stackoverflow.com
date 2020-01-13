f9a1a0ee8b79e02a173bfd2769b42fb3081f7223
codrelphi <chancerel.codjovi@gmail.com>
2020-01-13 22:22:49 +0100

    use a naive method based on the split method

--- python/remove_characters_from_dictionary_keys_values.py
+++ python/remove_characters_from_dictionary_keys_values.py
@@ -5,3 +5,19 @@
 # date: 2020-01-13
 # source: https://stackoverflow.com/questions/59723285/removing-characters-from-both-a-dictionarys-keys-and-values
 #=================================================================================
+def dot_be_gone(dirty_dic):
+    clean_dic = {}
+    for x, y in dirty_dic.items():
+        clean_x = x
+        clean_y = y
+        if '.' in x:
+            x_items = x.split('.')
+            clean_x = x_items[0] + x_items[1]
+        if '.' in y:
+            y_items = y.split('.')
+            clean_y = y_items[0] + y_items[1]
+        clean_dic[clean_x] = clean_y
+    return(clean_dic)
+
+x = dot_be_gone({"No. of Pets":"1", "No Problem":"Okay..."})
+print(x) # {'No of Pets': '1', 'No Problem': 'Okay'}
