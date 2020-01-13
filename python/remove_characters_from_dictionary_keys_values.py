84da64f56b4e54b48f5fca746f97631376dd5a6c
codrelphi <chancerel.codjovi@gmail.com>
2020-01-13 22:25:37 +0100

    use an efficient method based on 'dict comprehensions' to solve the problem

--- python/remove_characters_from_dictionary_keys_values.py
+++ python/remove_characters_from_dictionary_keys_values.py
@@ -6,12 +6,7 @@
 # source: https://stackoverflow.com/questions/59723285/removing-characters-from-both-a-dictionarys-keys-and-values
 #=================================================================================
 def dot_be_gone(dirty_dic):
-    clean_dic = {}
-    for x, y in dirty_dic.items():
-        clean_x = x.replace('.', "")
-        clean_y = y.replace('.', "")
-        clean_dic[clean_x] = clean_y
-    return(clean_dic)
+    return {k.replace('.', '') : v.replace('.', '') for k, v in dirty_dic.items()}
 
 x = dot_be_gone({"No. of Pets":"1", "No Problem":"Okay..."})
 print(x) # {'No of Pets': '1', 'No Problem': 'Okay'}
