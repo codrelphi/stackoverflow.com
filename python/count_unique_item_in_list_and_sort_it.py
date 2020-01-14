670343c0d863f1d9551b3d7ebefcc1c58334367b
codrelphi <chancerel.codjovi@gmail.com>
2020-01-14 18:44:04 +0100

    write the working code to achieve the two desired goals

--- python/count_unique_item_in_list_and_sort_it.py
+++ python/count_unique_item_in_list_and_sort_it.py
@@ -5,3 +5,23 @@
 # date: 2020-01-14
 # source: the post is deleted
 #=================================================================================
+the_list = ['Antonia', 'Sara', 'Nick', 'Deppy', 'Antonia', 'Deppy', 'Antonia']
+
+# count unique item in the list 'the_list'
+for item in set(the_list):
+    print(item, ':', the_list.count(item))
+
+# an ordered new list containing unique item of 'the_list'
+new_list = sorted([item for item in set(the_list)])
+print(new_list)
+"""
+> **Outputs:**
+
+    Nick : 1
+    Antonia : 3
+    Sara : 1
+    Deppy : 2
+
+    ['Antonia', 'Deppy', 'Nick', 'Sara']
+
+"""
