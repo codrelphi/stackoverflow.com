8fba9d5b6af58ebbe262e316786b0568929f1fec
codrelphi <chancerel.codjovi@gmail.com>
2020-01-15 23:00:41 +0100

    add my personal header

---
+++ python/making_function_to_change_a_variable.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2020-01-14
+# source: https://stackoverflow.com/questions/59739081/making-a-function-to-change-a-variable
+#=================================================================================
