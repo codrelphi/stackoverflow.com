31f1b5f70cc1ed3d473fadc42f352721b87a4ae2
codrelphi <chancerel.codjovi@gmail.com>
2020-01-15 23:01:34 +0100

    add the method 1

--- python/making_function_to_change_a_variable.py
+++ python/making_function_to_change_a_variable.py
@@ -5,3 +5,14 @@
 # date: 2020-01-14
 # source: https://stackoverflow.com/questions/59739081/making-a-function-to-change-a-variable
 #=================================================================================
+
+# Method 1: based on your code.
+def check(output_name):
+    if ".pdf" not in output_name:
+        output_name += ".pdf"
+    return output_name
+
+
+output_name = "sample"
+result = check(output_name)
+print(result) # sample.pdf
