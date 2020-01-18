de9272457e04b101dfa8ae3df8f4398681fee85d
codrelphi <chancerel.codjovi@gmail.com>
2020-01-18 21:51:52 +0100

    write the working code

--- java/welcome_to_java.java
+++ java/welcome_to_java.java
@@ -3,3 +3,11 @@
 * date: 2020-01-18
 * source: https://www.hackerrank.com/challenges/welcome-to-java/problem
 */
+
+public class Solution {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+  }
+}
